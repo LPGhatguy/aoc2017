d81031fcaff0c5a3d6957fe34d7b946a9bca6875
Lucien Greathouse <me@lpghatguy.com>
2017-12-08 23:58:50 -0800

    Day 5

---
+++ day-5.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const { readFileSync } = require("fs");
+
+const input = readFileSync("day-5-input.txt", "utf8");
+
+// const input = `
+// 0
+// 3
+// 0
+// 1
+// -3
+// `.trim();
+
+const values = input
+	.split("\n")
+	.map(line => parseInt(line.trim()));
+
+let cursor = 0;
+let stepCount = 0;
+
+while (true) {
+	const next = cursor + values[cursor];
+
+	if (values[cursor] >= 3) {
+		values[cursor]--;
+	} else {
+		values[cursor]++;
+	}
+
+	stepCount++;
+
+	if (next >= values.length) {
+		break;
+	}
+
+	cursor = next;
+}
+
+console.log(stepCount);
---
+++ day-5pt2.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const { readFileSync } = require("fs");
+
+const input = readFileSync("day-5-input.txt", "utf8");
+
+// const input = `
+// 0
+// 3
+// 0
+// 1
+// -3
+// `.trim();
+
+const values = input
+	.split("\n")
+	.map(line => parseInt(line.trim()));
+
+let cursor = 0;
+let stepCount = 0;
+
+while (true) {
+	const next = cursor + values[cursor];
+	values[cursor]++;
+	stepCount++;
+
+	if (next >= values.length) {
+		break;
+	}
+
+	cursor = next;
+}
+
+console.log(stepCount);
