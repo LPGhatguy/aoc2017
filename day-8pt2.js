b7f88e3703ee0dbe57ef10f91a4678f4a8877ec0
Lucien Greathouse <me@lpghatguy.com>
2017-12-11 23:19:03 -0800

    Day 8 part 2

---
+++ day-8pt2.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const { readFileSync } = require("fs");
+
+const input = readFileSync("day-8-input.txt", "utf8");
+
+const lineReg = /^(\S+) (\S+) (\S+) if (\S+) (\S+) (\S+)/;
+
+const conditions = {
+	"==": (a, b) => a == b,
+	">=": (a, b) => a >= b,
+	"<=": (a, b) => a <= b,
+	"<": (a, b) => a < b,
+	">": (a, b) => a > b,
+	"!=": (a, b) => a !== b,
+};
+
+const operators = {
+	inc: (a, b) => a + b,
+	dec: (a, b) => a - b,
+};
+
+let largestEver = -Infinity;
+
+const registers = input
+	.split("\n")
+	.reduce((registers, line) => {
+		const [, register, op, value, condRegister, condition, condValue] = line.match(lineReg);
+
+		if (conditions[condition](registers[condRegister] || 0, parseInt(condValue))) {
+			registers[register] = operators[op](registers[register] || 0, parseInt(value));
+
+			largestEver = Math.max(largestEver, registers[register]);
+		}
+
+		return registers;
+	}, {});
+
+let largest = 0;
+for (const key of Object.keys(registers)) {
+	console.log(key, registers[key]);
+	largest = Math.max(largest, registers[key]);
+}
+
+console.log(largest);
+console.log(largestEver);
