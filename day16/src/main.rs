fc7d37db32631ae864fd8e3a90ca5d68c1f2924e
Lucien Greathouse <me@lpghatguy.com>
2017-12-25 23:21:39 -0800

    Day 16, part 1

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,32 +1,70 @@
+#[macro_use]
+extern crate lazy_static;
 extern crate regex;
+
+use std::collections::HashMap;
 
 use regex::Regex;
 
-static input: &'static str = include_str!("input.txt");
+lazy_static! {
+    static ref VALUES: Vec<&'static str> = vec![
+        "a", "b", "c", "d", "e", "f", "g", "h",
+        "i", "j", "k", "l", "m", "n", "o", "p",
+    ];
 
-fn spin(list: &mut Vec<u64>, x: usize) {
+    static ref NAME_TO_NUM: HashMap<&'static str, usize> = {
+        let mut map = HashMap::new();
+
+        for (key, value) in VALUES.iter().enumerate() {
+            map.insert(*value, key);
+        }
+
+        map
+    };
+}
+
+static INPUT: &'static str = include_str!("input.txt");
+
+fn spin(list: &mut Vec<&'static str>, x: usize) {
+    let tail_len = list.len().saturating_sub(x);
+    let tail = list.split_off(tail_len);
+
+    for e in tail.iter().rev() {
+        list.insert(0, *e);
+    }
 }
 
 fn pt1() {
-	let spin_pattern = Regex::new(r"^s(\d+)").unwrap();
-	let exchange_pattern = Regex::new(r"^x(\d+)/(\d+)").unwrap();
-	let partner_pattern = Regex::new(r"^p([a-zA-Z])/([a-zA-Z])").unwrap();
+    let spin_pattern = Regex::new(r"^s(\d+)").unwrap();
+    let exchange_pattern = Regex::new(r"^x(\d+)/(\d+)").unwrap();
+    let partner_pattern = Regex::new(r"^p([a-zA-Z])/([a-zA-Z])").unwrap();
 
-	let values: Vec<u64> = (0..16).collect();
+    let mut values = VALUES.clone();
 
-	for instruction in input.split(",") {
-		if let Some(captures) = spin_pattern.captures(instruction) {
-			let value = captures.get(1).unwrap().as_str().parse::<u64>().unwrap();
+    for instruction in INPUT.split(",") {
+        if let Some(captures) = spin_pattern.captures(instruction) {
+            let value = captures.get(1).unwrap().as_str().parse::<usize>().unwrap();
 
-			println!("Spin {}", value);
-		} else if let Some(captures) = exchange_pattern.captures(instruction) {
+            spin(&mut values, value);
+        } else if let Some(captures) = exchange_pattern.captures(instruction) {
+            let a = captures.get(1).unwrap().as_str().parse::<usize>().unwrap();
+            let b = captures.get(2).unwrap().as_str().parse::<usize>().unwrap();
 
-		} else if let Some(captures) = partner_pattern.captures(instruction) {
+            values.swap(a, b);
+        } else if let Some(captures) = partner_pattern.captures(instruction) {
+            let a_name = captures.get(1).unwrap().as_str();
+            let b_name = captures.get(2).unwrap().as_str();
 
-		}
-	}
+            let a = values.iter().position(|v| *v == a_name).unwrap();
+            let b = values.iter().position(|v| *v == b_name).unwrap();
+
+            values.swap(a, b);
+        }
+    }
+
+    println!("{}", values.join(""));
 }
 
 fn main() {
-	pt1();
+    pt1();
 }
