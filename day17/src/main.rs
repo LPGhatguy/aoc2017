6907cba1a82000a0d50e8a01d22669fac0b08e52
Lucien Greathouse <me@lpghatguy.com>
2017-12-26 17:08:14 -0800

    Day 17 part two

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -6,13 +6,32 @@ fn pt1() {
     for next_value in 1..2018 {
         position = (position + cycle_count) % buffer.len() + 1;
         buffer.insert(position, next_value);
-
-        println!("Insert value {} at position {}", next_value, position);
     }
 
+    println!("First two values: {:?}", &buffer[0..2]);
     println!("Values around current position: {:?}", &buffer[(position - 1)..(position + 2)]);
 }
 
+fn pt2() {
+    let cycle_count = 312;
+    let mut position = 1;
+    let mut value_after_zero = 1;
+
+    for next_value in 2..(50_000_000 + 1) {
+        position = (position + cycle_count) % next_value + 1;
+
+        if position == 1 {
+            value_after_zero = next_value;
+        }
+    }
+
+    println!("Value after zero: {:?}", value_after_zero);
+}
+
 fn main() {
+    println!("Part one:");
     pt1();
+
+    println!("Part two:");
+    pt2();
 }
